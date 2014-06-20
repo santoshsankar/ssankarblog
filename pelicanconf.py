f2ed4097878ba1fe2c89ac9d597fad3d6ed7f360
Santosh Sankar <santoshsankar@gmail.com>
2014-06-20 18:08:29 -0400

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Santosh Sankar'
+SITENAME = u'Dynamic Thoughts for an Evolving World'
+SITEURL = ''
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+
+TIMEZONE = 'America/New_York'
+DATE_FORMATS = {
+    'en': '%a, %d %b %Y',
+}
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 4
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
